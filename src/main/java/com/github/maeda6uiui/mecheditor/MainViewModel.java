31cb46a2fee62be7513a2056d76803303df6f2a4
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-03-09 19:44:07 +0900

    Draw wireframe

--- src/main/java/com/github/maeda6uiui/mecheditor/MainViewModel.java
+++ src/main/java/com/github/maeda6uiui/mecheditor/MainViewModel.java
@@ -1,13 +1,18 @@
 package com.github.maeda6uiui.mecheditor;
 
+import com.github.dabasan.jxm.bd1.BD1Block;
+import com.github.dabasan.jxm.bd1.BD1Manipulator;
 import com.github.maeda6uiui.mechtatel.core.MttWindow;
 import com.github.maeda6uiui.mechtatel.core.camera.FreeCamera;
 import com.github.maeda6uiui.mechtatel.core.postprocessing.PostProcessingProperties;
 import com.github.maeda6uiui.mechtatel.core.postprocessing.light.ParallelLight;
 import com.github.maeda6uiui.mechtatel.core.screen.MttScreen;
 import com.github.maeda6uiui.mechtatel.core.screen.ScreenImageType;
+import com.github.maeda6uiui.mechtatel.core.screen.component.MttLineSet;
 import com.github.maeda6uiui.mechtatel.core.screen.component.MttModel;
 import com.github.maeda6uiui.mechtatel.core.screen.texture.MttTexture;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -24,6 +29,8 @@ public class MainViewModel {
     private FreeCamera camera;
 
     private MttModel model;
+    private List<BD1Block> blocks;
+    private MttLineSet blockEdges;
 
     public MainViewModel(MttWindow window, MttScreen imguiScreen) {
         screen = window.createScreen(
@@ -35,6 +42,10 @@ public class MainViewModel {
 
         PostProcessingProperties ppProps = screen.getPostProcessingProperties();
         ParallelLight light = ppProps.createParallelLight();
+
+        MttLineSet axis = screen.createLineSet();
+        axis.addAxes(5.0f);
+        axis.createBuffer();
 
         camera = new FreeCamera(screen.getCamera());
     }
@@ -48,8 +59,47 @@ public class MainViewModel {
         if (model != null) {
             model.cleanup();
         }
-
         model = screen.createModel(modelFile);
+        model.setVisible(false);
+
+        //Create line set for block edges
+        final float BD1_MODEL_SCALE = 1.7f / 20.0f;
+
+        var manipulator = new BD1Manipulator(modelFile.toFile());
+        manipulator.rescale(BD1_MODEL_SCALE, BD1_MODEL_SCALE, BD1_MODEL_SCALE).applyTransformation();
+        blocks = manipulator.getBlocks();
+
+        blockEdges = screen.createLineSet();
+        blocks.forEach(block -> {
+            //Up
+            for (int i = 0; i < 4; i++) {
+                blockEdges.add(
+                        new Vector3f(block.vertexPositions[i]),
+                        new Vector4f(1.0f),
+                        new Vector3f(block.vertexPositions[(i + 1) % 4]),
+                        new Vector4f(1.0f)
+                );
+            }
+            //Down
+            for (int i = 0; i < 4; i++) {
+                blockEdges.add(
+                        new Vector3f(block.vertexPositions[4 + i]),
+                        new Vector4f(1.0f),
+                        new Vector3f(block.vertexPositions[4 + (i + 1) % 4]),
+                        new Vector4f(1.0f)
+                );
+            }
+            //Vertical
+            for (int i = 0; i < 4; i++) {
+                blockEdges.add(
+                        new Vector3f(block.vertexPositions[i]),
+                        new Vector4f(1.0f),
+                        new Vector3f(block.vertexPositions[i + 4]),
+                        new Vector4f(1.0f)
+                );
+            }
+        });
+        blockEdges.createBuffer();
     }
 
     public void draw() {
